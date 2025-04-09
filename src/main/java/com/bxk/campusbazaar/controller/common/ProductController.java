65cd5571a03eafba034f1a5078577a6a8fa4b2d9
moyanislth <3094673523@qq.com>
2025-04-09 18:42:41 +0800

    xml中resultMap出错修复

--- src/main/java/com/bxk/campusbazaar/controller/common/ProductController.java
+++ src/main/java/com/bxk/campusbazaar/controller/common/ProductController.java
@@ -4,6 +4,7 @@ import com.bxk.campusbazaar.tools.Response;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class ProductController {
     @GetMapping("/getAllProducts")
     public Response<Object> getAllProducts(){
         return Response.success("获取所有商品成功");
+    }
+
+    @PostMapping("/addProduct")
+    public Response<Object> addProduct(){
+        return Response.success("添加商品成功");
     }
 }
